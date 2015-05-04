3299442270f74a6e7f0d955857bfa2da1117c149
Svenja Hilgen <shilgen@live.de>
2015-05-04 16:24:47 +0200

    Verbesserungen an der navbar

--- src/main/webapp/scripts/components/navbar/navbar.directive.js
+++ src/main/webapp/scripts/components/navbar/navbar.directive.js
@@ -8,9 +8,10 @@ angular.module('configeditorApp')
                 var language = attrs.activeMenu;
 
                 scope.$watch(function() {
-                    //return $translate.use();
+                    return $translate.use();
                 }, function(selectedLanguage) {
                     if (language === selectedLanguage) {
+                        tmhDynamicLocale.set(language);
                         element.addClass('active');
                     } else {
                         element.removeClass('active');
@@ -19,15 +20,14 @@ angular.module('configeditorApp')
             }
         };
     })
-    .directive('activeLink', function($location) {
+    .directive('activeLink', function(location) {
         return {
             restrict: 'A',
             link: function (scope, element, attrs) {
                 var clazz = attrs.activeLink;
                 var path = attrs.href;
                 path = path.substring(1); //hack because path does bot return including hashbang
-                debugger;
-                scope.location = $location;
+                scope.location = location;
                 scope.$watch('location.path()', function(newPath) {
                     if (path === newPath) {
                         element.addClass(clazz);
