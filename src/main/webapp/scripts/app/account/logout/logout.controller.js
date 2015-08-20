10b6cc6ce8f74235c1c898bb8b2b094747f15a6b
Svenja Hilgen <shilgen@live.de>
2015-08-20 14:25:31 +0200

    Deleted unnecessary files

--- src/main/webapp/scripts/app/account/logout/logout.controller.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('configeditorApp')
-    .controller('LogoutController', function (Auth) {
-        Auth.logout();
-    });
--- src/main/webapp/scripts/app/main/main.controller.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('configeditorApp')
-    .controller('MainController', function ($scope) {
-
-    });
